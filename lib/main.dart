e49f04f5e86e6369189386fa9fc684b6f0ef1bd4
seoyun_iMac <nuyoes1618@gmail.com>
2024-08-25 02:41:25 +0900

    chore: getIt을 통한 의존성 주입 #11

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:kobot_client/app.dart';
+import 'package:kobot_client/config/di_setup.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Flutter 초기화 보장
+  await diSetup(); // 의존성 주입 설정
   runApp(const MyApp());
 }
